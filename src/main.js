d1870a846b55f3b8526817fc928979acc9ba3055
Soroush4208 <soroush.r80@gmail.com>
2025-11-01 23:51:28 +0330

    feat : create city page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { LoginPage } from './components/login/Login';
+// import { LoginPage } from './components/login/Login';
+import { CityList } from './components/CityList/CityList';
 import './style.css';
 
 // document.querySelector('#app').append(LoginPage());
-document.querySelector('#app').append(LoginPage());
+document.querySelector('#app').append(CityList());
